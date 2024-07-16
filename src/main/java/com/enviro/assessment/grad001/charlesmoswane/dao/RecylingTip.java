c99887b4a4a74af1ebc6f2532a7f454f265b2e4f
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-16 23:56:49 +0200

    feat: Use lombok to generate getters and setters

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecylingTip.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecylingTip.java
@@ -1,5 +1,8 @@
 package com.enviro.assessment.grad001.charlesmoswane.dao;
 
+import lombok.Data;
+
+@Data
 public class RecylingTip {
     private int id;
     private String name;
@@ -19,27 +22,4 @@ public class RecylingTip {
         this.description = description;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
