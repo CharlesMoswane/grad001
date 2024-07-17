d39dc6bd3a88f97e4f944b24136d896f04a59f7d
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-17 21:59:29 +0200

    build: Add Maven dependency for JpaRepository

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/DisposalGuidelineRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/DisposalGuidelineRepository.java
@@ -1,6 +1,7 @@
 package com.enviro.assessment.grad001.charlesmoswane.dao;
 
 import com.enviro.assessment.grad001.charlesmoswane.entity.DisposalGuideline;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Integer> {
 }
