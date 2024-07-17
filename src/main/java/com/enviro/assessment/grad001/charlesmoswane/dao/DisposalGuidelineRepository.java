ed332f451a43faf9742d2706b6a4c923a12bf4b0
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-17 21:38:57 +0200

    feat: Add disposal guidelines dao

---
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/DisposalGuidelineRepository.java
@@ -0,0 +1,6 @@
+package com.enviro.assessment.grad001.charlesmoswane.dao;
+
+import com.enviro.assessment.grad001.charlesmoswane.entity.DisposalGuideline;
+
+public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Integer> {
+}
