776a3f3ec615c999a1e9fc4daf9b0072ee732398
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-17 22:26:33 +0200

    feat: Expose the repos as REST resources with custom paths

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/DisposalGuidelineRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/DisposalGuidelineRepository.java
@@ -2,6 +2,8 @@ package com.enviro.assessment.grad001.charlesmoswane.dao;
 
 import com.enviro.assessment.grad001.charlesmoswane.entity.DisposalGuideline;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "disposalGuidelines", path = "disposalGuidelines")
 public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Integer> {
 }
--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecyclingTipRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecyclingTipRepository.java
@@ -1,6 +1,8 @@
 package com.enviro.assessment.grad001.charlesmoswane.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "recyclingTips", path = "recyclingTips")
 public interface RecyclingTipRepository extends JpaRepository<RecyclingTipRepository, Integer> {
 }
