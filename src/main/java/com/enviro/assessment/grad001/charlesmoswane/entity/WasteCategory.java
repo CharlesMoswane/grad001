a76009e7229cefd0beda4b7d7d15efb1281e04c3
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-18 17:40:00 +0200

    feat: Remove redundant WasteCategory constructor

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/WasteCategory.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/WasteCategory.java
@@ -11,19 +11,4 @@ public class WasteCategory {
     private int id;
     private String name;
     private String description;
-
-    public WasteCategory() {
-    }
-
-    public WasteCategory(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public WasteCategory(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
 }
