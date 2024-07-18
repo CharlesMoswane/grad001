a1ba85ea337661c819bdfcbf32cec4a0496d88df
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-18 17:00:00 +0200

    feat: Add h2 db configs

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/WasteCategory.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/WasteCategory.java
@@ -1,9 +1,13 @@
 package com.enviro.assessment.grad001.charlesmoswane.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity
 @Data
 public class WasteCategory {
+    @Id
     private int id;
     private String name;
     private String description;
