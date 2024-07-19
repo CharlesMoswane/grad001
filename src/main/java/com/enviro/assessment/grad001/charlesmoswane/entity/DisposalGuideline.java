ffe66e31d343dfa66c5d430df68240bd9e3c3114
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-19 17:20:00 +0200

    feat: Add auto generated primary keys

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/DisposalGuideline.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/DisposalGuideline.java
@@ -1,6 +1,8 @@
 package com.enviro.assessment.grad001.charlesmoswane.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,7 @@ import lombok.Data;
 @Data
 public class DisposalGuideline {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
