ca7d4c1eca98fa7ee0c2500fd1d0c92b475a9640
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-19 17:10:00 +0200

    fix: JpaRepository parameters

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecyclingTipRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/RecyclingTipRepository.java
@@ -1,8 +1,9 @@
 package com.enviro.assessment.grad001.charlesmoswane.dao;
 
+import com.enviro.assessment.grad001.charlesmoswane.entity.RecyclingTip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "recyclingTips", path = "recyclingTips")
-public interface RecyclingTipRepository extends JpaRepository<RecyclingTipRepository, Integer> {
+public interface RecyclingTipRepository extends JpaRepository<RecyclingTip, Integer> {
 }
--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/WasteCategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/dao/WasteCategoryRepository.java
@@ -1,8 +1,9 @@
 package com.enviro.assessment.grad001.charlesmoswane.dao;
 
+import com.enviro.assessment.grad001.charlesmoswane.entity.WasteCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "wasteCategories", path = "wasteCategories")
-public interface WasteCategoryRepository extends JpaRepository<WasteCategoryRepository, Integer> {
+public interface WasteCategoryRepository extends JpaRepository<WasteCategory, Integer> {
 }
