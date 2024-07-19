5a52a09c2d337673a9ef0ee5eac3460fea931ade
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-07-19 17:00:00 +0200

    feat: Remove redundant RecyclingTip constructor

--- src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/RecyclingTip.java
+++ src/main/java/com/enviro/assessment/grad001/charlesmoswane/entity/RecyclingTip.java
@@ -11,19 +11,4 @@ public class RecyclingTip {
     private int id;
     private String name;
     private String description;
-
-    public RecyclingTip() {
-    }
-
-    public RecyclingTip(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public RecyclingTip(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
 }
